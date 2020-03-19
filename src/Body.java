2ba6359a6955899b2f94cdfb5189d72701556246
vanduc2514 <vanduc2514@gmail.com>
2020-03-19 18:03:51 +0700

    Refactor

--- src/Body.java
+++ src/Body.java
@@ -5,16 +5,16 @@ public class Body {
         Scanner scanner = new Scanner(System.in);
 
         double weight;
-        System.out.println("Enter your weight: ");
+        System.out.println("Enter your weight (in kilogram):");
         weight = scanner.nextDouble();
 
         double height;
-        System.out.println("Enter your height");
+        System.out.println("Enter your height (in meter):");
         height = scanner.nextDouble();
 
         double bmi;
         bmi = weight / Math.pow(height, 2);
-        System.out.printf("Your bmi is %f \n \n", bmi);
+        System.out.printf("Your bmi is %3.2f \n \n", bmi);
 
         String result;
         if (bmi < 18.5) {
@@ -27,6 +27,6 @@ public class Body {
             result = "Obese";
         }
 
-        System.out.printf("You are: %s \n", result);
+        System.out.printf("%-4.2f %s", bmi, "is evaluated as " + result);
     }
 }
